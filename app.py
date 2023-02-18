b23724060c087f6fe32d3d6e41bd63c06947b3fa
Dre Dyson <35677602+ergosumdre@users.noreply.github.com>
2023-02-17 19:32:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,18 +23,17 @@ def language_model(input_prompt, input_numBeams, input_min_text_length, input_ma
                no_repeat_ngram_size=2, \
                early_stopping=True, \
                temperature=temp)
-  #output_text_Flan_t5 = tokenizer.batch_decode(outputs, \
-  #                      skip_special_tokens=True)
   return tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
 
 language_model_UI = gr.Interface(fn=language_model,
-                               inputs=[gr.Textbox(lines = 1, placeholder="Enter your prompt..."),
-                                       gr.Slider(10, 100),
-                                       gr.Slider(20, 5000),
-                                       gr.Slider(20, 5000),
-                                       gr.Slider(0, 10),
-                                       gr.Slider(0, 1)],
+                               inputs=[gr.Textbox(lines = 1, placeholder="Enter your prompt...", label="Prompt"), # input_prompt
+                                       gr.Slider(minimum=1, maximum=50, value=16, label="Number of Beams"), # input_numBeams
+                                       gr.Slider(minimum=20, maximum=5000, value=100, step=50, label="Minimum text length"), # input_min_text_length
+                                       gr.Slider(minimum=21, maximum=1000, value=250, step=50, label="Maximum text length"), # input_max_length
+                                       gr.Slider(minimum=0, maximum=10, value=2, step=1, label="Penalty length"), # input_length_penalty
+                                       gr.Slider(minimum=0, maximum=1, value=.8, step=.1, label="Temperature") # input_temp
+                                      ],
                                outputs="text",
                                title = "Dre's ChatGPT")
 
